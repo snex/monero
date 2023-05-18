04921207f9ed5cb826b64f3945071c633b94611c
David Havlicek <snex00@protonmail.com>
2023-05-17 18:49:42 -0700

    wtf

--- lib/monero_rpc/config.rb
+++ lib/monero_rpc/config.rb
@@ -2,7 +2,7 @@ require 'singleton'
 module MoneroRPC
   class Config
     include Singleton
-    attr_accessor :host, :port, :username, :password, :debug, :in_transfer_clazz, :out_transfer_clazz
+    attr_accessor :host, :port, :username, :password, :debug, :in_transfer_clazz, :out_transfer_clazz, :pool_transfer_clazz
 
     def initialize
       self.in_transfer_clazz = "MoneroRPC::IncomingTransfer"
